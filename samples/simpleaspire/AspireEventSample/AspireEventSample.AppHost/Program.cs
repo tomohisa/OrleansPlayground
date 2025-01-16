1aa6cec257b3a477ed43e2eaeb5b91e5829480ee
Tomohisa Takaoka <ttakaoka@jtechs.com>
2025-01-16 10:02:17 -0800

    make it to run orleans

---
+++ samples/simpleaspire/AspireEventSample/AspireEventSample.AppHost/Program.cs
@@ -0,0 +1,20 @@
+var builder = DistributedApplication.CreateBuilder(args);
+
+var storage = builder.AddAzureStorage("azurestorage").RunAsEmulator(r => r.WithImage("azure-storage/azurite", "3.33.0" ) );
+var clusteringTable = storage.AddTables("clustering");
+var grainStorage = storage.AddBlobs("grain-state");
+
+var orleans = builder.AddOrleans("default")
+    .WithClustering(clusteringTable)
+    .WithGrainStorage("Default", grainStorage);
+
+var apiService = builder.AddProject<Projects.AspireEventSample_ApiService>("apiservice")
+    .WithExternalHttpEndpoints()
+    .WithReference(orleans);
+
+builder.AddProject<Projects.AspireEventSample_Web>("webfrontend")
+    .WithExternalHttpEndpoints()
+    .WithReference(apiService)
+    .WaitFor(apiService);
+
+builder.Build().Run();
