4c264d365363d8476edff92f331c0674870e492d
Tomohisa Takaoka <ttakaoka@jtechs.com>
2025-01-18 00:59:25 -0800

    api 接続前

--- samples/AspireEventSample/AspireEventSample.ApiService/Grains/IAggregateProjectorGrain.cs
+++ samples/AspireEventSample/AspireEventSample.ApiService/Grains/IAggregateProjectorGrain.cs
@@ -4,6 +4,9 @@ using Sekiban.Pure.Aggregates;
 using Sekiban.Pure.Command.Executor;
 using Sekiban.Pure.Command.Handlers;
 using Sekiban.Pure.Documents;
+using Sekiban.Pure.Events;
+using Sekiban.Pure.Projectors;
+using Sekiban.Pure.Repositories;
 
 namespace AspireEventSample.ApiService.Grains;
 
@@ -33,27 +36,44 @@ public interface IAggregateProjectorGrain : IGrainWithStringKey
     Task<IAggregatePayload> RebuildStateAsync();
 }
 
-public class AggregateProjectorGrain : Grain, IAggregateProjectorGrain
+public class AggregateProjectorGrain(
+    [PersistentState(stateName: "aggregate", storageName: "projected")] IPersistentState<Aggregate> state) : Grain, IAggregateProjectorGrain
 {
     public async Task<IAggregate> GetStateAsync()
     {
-        throw new System.NotImplementedException();
+        var partitionKeysAndProjector = PartitionKeysAndProjector.FromGrainKey(this.GetPrimaryKeyString()).UnwrapBox();
+        return Repository.Load(partitionKeysAndProjector.PartitionKeys, partitionKeysAndProjector.Projector).UnwrapBox();
     }
-
     public async Task<CommandResponse> ExecuteCommandAsync(ICommandWithHandlerSerializable command)
     {
-        var projector = command.GetProjector();
-        var handler = command.GetHandler();
-        var partitionKeysSpecifier = command.GetPartitionKeysSpecifier();
+        var partitionKeysAndProjector = PartitionKeysAndProjector.FromGrainKey(this.GetPrimaryKeyString()).UnwrapBox();
+        this.GetPrimaryKeyString();
         var commandExecutor = new CommandExecutor();
-        var aggregateType = command.GetAggregatePayloadType();
-        var result = await commandExecutor.ExecuteGeneralNonGeneric(command, projector, partitionKeysSpecifier, NoInjection.Empty, handler, aggregateType);
+        var result = await commandExecutor.ExecuteGeneralNonGeneric(command, partitionKeysAndProjector.Projector, partitionKeysAndProjector.PartitionKeys, NoInjection.Empty, command.GetHandler(), command.GetAggregatePayloadType());
+        var aggregate = Repository.Load(partitionKeysAndProjector.PartitionKeys, partitionKeysAndProjector.Projector).UnwrapBox();
+        state.State = aggregate;
+        await state.WriteStateAsync();
         return result.UnwrapBox();
     }
 
     public async Task<IAggregatePayload> RebuildStateAsync()
     {
         throw new System.NotImplementedException();
+    }
+}
+
+public record PartitionKeysAndProjector(PartitionKeys PartitionKeys, IAggregateProjector Projector)
+{
+    public static ResultBox<PartitionKeysAndProjector> FromGrainKey(string grainKey)
+    {
+        var splitted = grainKey.Split("=");
+        if (splitted.Length != 2)
+        {
+            throw new ResultsInvalidOperationException("invalid grain key");
+        }
+        var partitionKeys = PartitionKeys.FromPrimaryKeysString(splitted[0]).UnwrapBox();
+        var projectorSpecifier = new MyAggregateProjectorSpecifier();
+        return projectorSpecifier.GetProjector(splitted[1]).Remap(projector => new PartitionKeysAndProjector(partitionKeys, projector));
     }
 }
 
@@ -78,7 +98,43 @@ public static class OrleansResultBoxExtensions
 }
 
 [GenerateSerializer]
-public record UserAggregatePayload(string Name, int Age) : IAggregatePayload;
+public record Branch(string Name) : IAggregatePayload;
+
+public record BranchCreated(string Name) : IEventPayload;
+public record BranchNameChanged(string Name) : IEventPayload;
+[GenerateSerializer]
+public record RegisterBranch(string Name) : ICommandWithHandler<RegisterBranch, BranchProjector>
+{
+    public PartitionKeys SpecifyPartitionKeys(RegisterBranch command) => PartitionKeys<BranchProjector>.Generate();
+    public ResultBox<EventOrNone> Handle(RegisterBranch command, ICommandContext<IAggregatePayload> context) =>
+        EventOrNone.Event(new BranchCreated(command.Name));
+}
+
+public interface IAggregateProjectorSpecifier
+{
+    ResultBox<IAggregateProjector> GetProjector(string projectorName);
+}
+public class MyAggregateProjectorSpecifier : IAggregateProjectorSpecifier
+{
+    public ResultBox<IAggregateProjector> GetProjector(string projectorName)
+    {
+        return projectorName switch
+        {
+            nameof(BranchProjector) => new BranchProjector(),
+            _ => new ResultsInvalidOperationException("unknown projector")
+        };
+    }
+}
+public class BranchProjector : IAggregateProjector
+{
+    public IAggregatePayload Project(IAggregatePayload payload, IEvent ev) =>
+        (payload, ev.GetPayload()) switch
+        {
+            (EmptyAggregatePayload, BranchCreated created) => new Branch(created.Name),
+            (Branch branch, BranchNameChanged changed) => new Branch(changed.Name),
+            _ => payload
+        };
+}
 
 [GenerateSerializer]
 record OrleansAggregate<TAggregatePayload>(
@@ -92,12 +148,3 @@ record OrleansAggregate<TAggregatePayload>(
         return Payload;
     }
 }
-
-public class pr
-{
-    public async Task main()
-    {
-        new OrleansAggregate<UserAggregatePayload>(new UserAggregatePayload("Alice", 20), PartitionKeys.Generate(), 0, string.Empty);
-    }
-}
-    
