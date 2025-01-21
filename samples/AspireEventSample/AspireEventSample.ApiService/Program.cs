4ee1b371e2ae1afe07e8be4433eb064479e89b42
Tomohisa Takaoka <ttakaoka@jtechs.com>
2025-01-20 21:26:50 -0800

    Add Dashboard

--- samples/AspireEventSample/AspireEventSample.ApiService/Program.cs
+++ samples/AspireEventSample/AspireEventSample.ApiService/Program.cs
@@ -23,7 +23,7 @@ builder.Services.AddOpenApi();
 // orleans related integrations
 builder.AddKeyedAzureTableClient("clustering");
 builder.AddKeyedAzureBlobClient("grain-state");
-builder.UseOrleans();
+builder.UseOrleans(config => config.UseDashboard(options => {  }));
 
 builder.Services.AddSingleton(new SekibanTypeConverters(new AspireEventSampleApiServiceAggregateTypes(),
     new AspireEventSampleApiServiceEventTypes()));
