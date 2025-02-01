f23c5b9d4b163c55746d72d644bdca4ac0249dfb
Tomohisa Takaoka <ttakaoka@jtechs.com>
2025-02-01 00:11:31 -0800

    working on the event writer wip

--- samples/AspireEventSample/Sekiban.Pure/Exception/SekibanAggregateTypeRestrictionException.cs
+++ samples/AspireEventSample/Sekiban.Pure/Exception/SekibanAggregateTypeRestrictionException.cs
@@ -2,3 +2,6 @@ namespace Sekiban.Pure.Exception;
 
 public class SekibanAggregateTypeRestrictionException(string message)
     : ApplicationException(message), ISekibanException;
+
+public class SekibanConfigurationException(string message)
+    : ApplicationException(message), ISekibanException;
