e2627bbad2b68e8b73a34d254aa20cacdc385527
Tomohisa Takaoka <ttakaoka@jtechs.com>
2025-02-12 00:13:19 -0800

    Add unit test for InMemorySekibanExecutor and update using directives

--- samples/AspireEventSample/AspireEventSample.UnitTest/UnitTest1.cs
+++ samples/AspireEventSample/AspireEventSample.UnitTest/UnitTest1.cs
@@ -1,6 +1,9 @@
-﻿using AspireEventSample.ApiService.Projections;
+﻿using AspireEventSample.ApiService.Aggregates.Branches;
+using AspireEventSample.ApiService.Generated;
+using AspireEventSample.ApiService.Projections;
+using Sekiban.Pure.Command.Handlers;
+using Sekiban.Pure.Executors;
 using Sekiban.Pure.Query;
-
 namespace AspireEventSample.UnitTest;
 
 public class UnitTest1
@@ -12,5 +15,23 @@ public class UnitTest1
         var queryResult = new QueryResult<bool>(true);
         var general = queryResult.ToGeneral(query);
         Assert.True((bool)general.GetValue());
+    }
+
+    [Fact]
+    public async Task InMemorySetupTest()
+    {
+        var executor = new InMemorySekibanExecutor(
+            new AspireEventSampleApiServiceEventTypes(),
+            new FunctionCommandMetadataProvider(() => "test"),
+            new AspireEventSampleApiServiceQueryTypes(),
+            new AspireEventSampleApiServiceMultiProjectorType());
+
+        var result1 = await executor.ExecuteCommandAsync(new RegisterBranch("DDD"));
+        Assert.True(result1.IsSuccess);
+        var value = result1.GetValue();
+        Assert.NotNull(value);
+        Assert.Equal(1, value.Version);
+        var aggregateId = value.PartitionKeys.AggregateId;
+        Assert.NotEqual(Guid.Empty, aggregateId);
     }
 }
