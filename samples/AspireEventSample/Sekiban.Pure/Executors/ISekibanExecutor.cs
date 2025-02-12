caf82a0589bced974c9619d9d03611dc58e8795d
Tomohisa Takaoka <ttakaoka@jtechs.com>
2025-02-12 00:06:53 -0800

    Add GetMultiProjector methods for IQueryCommon and IListQueryCommon in QueryExecutionExtensionGenerator

--- samples/AspireEventSample/Sekiban.Pure/Executors/ISekibanExecutor.cs
+++ samples/AspireEventSample/Sekiban.Pure/Executors/ISekibanExecutor.cs
@@ -4,6 +4,7 @@ using Sekiban.Pure.Command.Handlers;
 using Sekiban.Pure.Events;
 using Sekiban.Pure.Projectors;
 using Sekiban.Pure.Query;
+using Sekiban.Pure.Repositories;
 namespace Sekiban.Pure.Executors;
 
 public interface ISekibanExecutor
@@ -41,12 +42,42 @@ public class InMemorySekibanExecutor(
                 : metadataProvider.GetMetadataWithSubscribedEvent(relatedEvent));
         return result;
     }
-    public Task<ResultBox<TResult>> ExecuteQueryAsync<TResult>(IQueryCommon<TResult> queryCommon)
-        where TResult : notnull =>
-        // if (queryCommon is IMultiprojector)
-        // queryTypes.execut
-        // var queryExecutor = new QueryExecutor();
-        throw new NotImplementedException();
+    public async Task<ResultBox<TResult>> ExecuteQueryAsync<TResult>(IQueryCommon<TResult> queryCommon)
+        where TResult : notnull
+    {
+        var projectorResult = queryTypes.GetMultiProjector(queryCommon);
+        if (projectorResult.IsSuccess)
+        {
+            var projector = projectorResult.GetValue();
+            var events = Repository.Events;
+            var projectionResult = events
+                .ToResultBox()
+                .ReduceEach(projector, (ev, proj) => multiProjectorsType.Project(proj, ev));
+            if (projectionResult.IsSuccess)
+            {
+                var projection = projectionResult.GetValue();
+                var lastEvent = events.LastOrDefault();
+                var multiProjectionState = new MultiProjectionState(
+                    projection,
+                    lastEvent?.Id ?? Guid.Empty,
+                    lastEvent?.SortableUniqueId ?? "",
+                    events.Count,
+                    0,
+                    lastEvent?.PartitionKeys.RootPartitionKey ?? "default");
+                var typedMultiProjectionState = multiProjectorsType.ToTypedState(multiProjectionState);
+                var queryExecutor = new QueryExecutor();
+                var queryResult = await queryTypes.ExecuteAsQueryResult(
+                    queryCommon,
+                    selector => typedMultiProjectionState
+                        .ToResultBox()
+                        .ConveyorWrapTry(state => state)
+                        .ToTask());
+                return queryResult.ConveyorWrapTry(val => (TResult)val.GetValue());
+            }
+            return ResultBox<TResult>.Error(new ApplicationException("Projection failed"));
+        }
+        return ResultBox<TResult>.Error(new ApplicationException("Projector not found"));
+    }
     public Task<ResultBox<ListQueryResult<TResult>>> ExecuteQueryAsync<TResult>(IListQueryCommon<TResult> queryCommon)
         where TResult : notnull => throw new NotImplementedException();
 }
